7e28d9a509cd6ea245e793706781c719c45f4169
dzinrai <dzinrai@mail.ru>
2020-02-19 22:45:38 +0300

    feat: implement count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let count = 0;
+  for (let x = 0; x < backyard.length; x++) {
+    for (let i = 0; i < backyard[x].length; i++) {
+      if( backyard[x][i] === '^^' ) {
+        count += 1;
+      } 
+    }    
+  }
+  return count;
 };
