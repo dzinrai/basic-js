f45d8537b85125f8cfe68f4813e28e3e91e1f615
dzinrai <dzinrai@mail.ru>
2020-02-23 07:13:37 +0300

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let coolName = [];
+  if( Array.isArray(arr) ){
+    coolName =  [...arr.filter( (x) => { return typeof(x) === "string"; } )];
+  }else{
+    return false;
+  }
+  coolName = coolName.map( (x) => { return x.trim().toUpperCase().match(/^[A-Z]/i); } );
+  return coolName.sort().join('');
 };
