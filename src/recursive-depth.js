be2b03fe68aa3e65edb34238fdbe46dbc30f781c
dzinrai <dzinrai@mail.ru>
2020-02-29 14:50:43 +0300

    feat: implement recursive-depth and vigenere-cipher

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,18 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(){
+        this.max = this.max.bind(this);
     }
+    calculateDepth(arr) {
+        if( Array.isArray(arr) ){
+            let z = this.max(arr.map( x => this.calculateDepth(x)));
+            return 1 + (z? z : 0);
+        }else if( !Array.isArray(arr) ){
+            return 0;
+        }
+    }
+    max(list){
+        if( !Array.isArray(list) ) return list;
+        list.sort( function(a,b) {return b-a;});
+        return list[0];
+      }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -13,16 +13,26 @@ class VigenereCipheringMachine {
         if(!message || !key) throw Error;
         message = message.toUpperCase();
         key = key.toUpperCase();
-        //console.log(table);
         let x,y;
         let enc_txt = "";
         let j = 0;
+        let new_gen_key = "";
+        for ( let letter of message) {
+            // генерируем ключ на основе сообщения, исключая все кроме самого сообщения 
+            // ( meassage = "123MESSAGE123", key ="SEVEN", new_gen_key="SEVENSE")
+            if(/[A-Z]/i.test(letter)){
+                new_gen_key += key.substr(j,1);
+                j = (j===key.length-1) ? 0 : j+1; // индекс j содержит значение которое сбрасывается если ключ надо расширять
+            }
+        }
+        if(this.reverse) new_gen_key=new_gen_key.split('').reverse().join('');
+        j = 0;
         for ( let i=0; i<message.length;i++) {
             if(/[A-Z]/g.test(message[i])){
                 x = this.alph.indexOf(message[i]);
-                y = this.alph.indexOf(key[j]);
+                y = this.alph.indexOf(new_gen_key[j]);
                 enc_txt += this.table[y][x];
-                j = (j===key.length-1) ? 0 : j+1;
+                j+=1;
             }else{
                 enc_txt += message[i];
             }
@@ -37,14 +47,23 @@ class VigenereCipheringMachine {
         let x,y;
         let dec_txt = "";
         let j = 0;
+        let new_gen_key = "";
+        for ( let letter of encryptedMessage){
+            // генерируем ключ на основе сообщения, исключая все кроме самого сообщения 
+            // ( encryptedMessage = "123MESSAGE123", key ="SEVEN", new_gen_key="SEVENSE")
+            if(/[A-Z]/i.test(letter)){
+                new_gen_key += key.substr(j,1);
+                j = (j===key.length-1) ? 0 : j+1; // индекс j содержит значение которое сбрасывается если ключ надо расширять
+            }
+        }        
+        j=0;
         for ( let i=0; i<encryptedMessage.length;i++) {
-            if(/[A-Z]/g.test(encryptedMessage[i])){
-                x = this.alph.indexOf(key[j]);
+            if(/[A-Z]/i.test(encryptedMessage[i])){
+                x = this.alph.indexOf(new_gen_key[j]);
                 let line = this.table[x];
                 y = line.indexOf(encryptedMessage[i]);
                 dec_txt += this.alph[y];
-
-                j = (j===key.length-1) ? 0 : j+1;
+                j+=1;
             }else{
                 dec_txt += encryptedMessage[i];
             }
